72f9fe54e22d6c97009c0cf767aa40b21f67acbb
Heather <hlmathies@gmail.com>
2018-03-01 21:47:32 -0500

    Add css for contact me form

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+$( document ).ready(function() {
+
+       $('fieldset').submit(function(event) {
+           event.preventDefault();
+
+           $.ajax({
+               url: "https://formspree.io/hlmathies@gmail.com",
+               method: "POST",
+               dataType: "json",
+               data: {
+                   name: $('#js-name').val(),
+                   email: $('#js-email').val(),
+                   message: $('#js-message').val()
+               },
+               success: function(data){
+                   $("fieldset").trigger("reset"); //reset form
+                   alert("Thanks for getting in touch.  I appreciate you!");
+
+               }
+       });
+
+   });
+
+ });
